a763f27f49be907f9b35eeb7629408e32932fd7c
GaliullinAR <galiullin.ar651@gmail.com>
2022-09-11 19:46:09 +0300

    eslint fix

--- src/__tests__/health-test.js
+++ src/__tests__/health-test.js
@@ -1,10 +1,9 @@
 import healthFunc from '../health';
 
 test.each([
-    [{ name: 'мечник', health: 10 }, 'critical'],
-    [{name: 'маг', health: 100}, 'healthy']
-  ])
-  ('Тестировние корректного отображения здоровья', (fn, expected) => {
-    const result = healthFunc(fn);
-    expect(result).toBe(expected);
-  })
+  [{ name: 'мечник', health: 10 }, 'critical'],
+  [{ name: 'маг', health: 100 }, 'healthy'],
+])('Тестировние корректного отображения здоровья', (fn, expected) => {
+  const result = healthFunc(fn);
+  expect(result).toBe(expected);
+});
